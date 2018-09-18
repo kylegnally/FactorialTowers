288cc245597b7c972d86a05f98be43d3f718aee6
David Barnes <dbarnes@kvcc.edu>
2018-09-18 17:13:39 -0400

    Add Factorial class and code to Main to use it.

---
+++ cis237-inclass2/Factorial.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237_inclass2
+{
+    class Factorial
+    {
+        public int SolveFactorial(int number)
+        {
+            // This is the base case
+            // If the number is one, just return it.
+            if (number == 1)
+            {
+                return 1;
+            }
+            // Not the base case, so make a recursive call
+            // to ourself
+            return number * SolveFactorial(number - 1);
+        }
+    }
+}
--- cis237-inclass2/Program.cs
+++ cis237-inclass2/Program.cs
@@ -10,6 +10,18 @@ namespace cis237_inclass2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter 1 for factorial or 2 for Tower of Hanoi");
+            string userInput = Console.ReadLine();
+
+            if (userInput == "1")
+            {
+                Factorial factorial = new Factorial();
+
+                Console.WriteLine("Solve 5!");
+                Console.Write("The answer is: ");
+                // 33 will overflow and give a negative answer
+                Console.WriteLine(factorial.SolveFactorial(5));
+            }
         }
     }
 }
